c75636478c8ecc8d50d596126bc5ce819536b22f
Cristinalvay <cristinalvay@gmail.com>
2026-01-22 13:19:54 +0100

    Add files via upload

---
+++ src/__init__.py
@@ -0,0 +1,16 @@
+from .common import build_hamiltonian_C, four_partition, inverse_check_cond, estimate_kappa
+from .method_m import solve_m_method, MMethodResult
+from .method_v import assemble_stack, solve_v_method, VSystem, VMethodResult
+
+__all__ = [
+    "build_hamiltonian_C",
+    "four_partition",
+    "inverse_check_cond",
+    "estimate_kappa",
+    "solve_m_method",
+    "MMethodResult",
+    "assemble_stack",
+    "solve_v_method",
+    "VSystem",
+    "VMethodResult",
+]
